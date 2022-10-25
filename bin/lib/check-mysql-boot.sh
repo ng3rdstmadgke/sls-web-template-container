624723e1f957f7c453e61c00ddb0ed35559be586
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2022-10-25 15:01:20 +0900

    mysqlのポートを変更

--- bin/lib/check-mysql-boot.sh
+++ bin/lib/check-mysql-boot.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-while :; do
-  success=$(MYSQL_PWD=$DB_PASSWORD mysql -u $DB_USER -h 127.0.0.1 $DB_NAME -e "SELECT 'success'" >/dev/null 2>&1; echo $?)
+for i in $(seq 1 25); do
+  success=$(MYSQL_PWD=$DB_PASSWORD mysql -u $DB_USER -h 127.0.0.1 -P $DB_PORT $DB_NAME -e "SELECT 'success'" >/dev/null 2>&1; echo $?)
   if [ "$success" = "0" ]; then
     echo "success!!"
-    break
+    exit
   else
     echo "mysql booting..."
   fi
   sleep 1
 done
+echo "failed..."
+exit 1
