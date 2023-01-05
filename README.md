8f48d36e0f962961c1bafbe061260bf818f6e12c
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2023-01-05 22:25:47 +0900

    slsをコンテナ化

--- README.md
+++ README.md
@@ -5,8 +5,6 @@
   - Secrets Manager
     - RDSの接続情報
     - JWTの秘密鍵
-- slsコマンドコンテナ化
-  - devコンテナに組み込む
 
 # インストール
 
@@ -34,20 +32,20 @@ npm install
 ```bash
 STAGE_NAME=mi1
 
-# /sls-web-template/lambda/ステージ名 で ECR リポジトリ作成
+# ${APP_NAME}/lambda/ステージ名 で ECR リポジトリ作成
 
 # イメージのビルドとpush
 ./bin/push-image.sh -s $STAGE_NAME
 
 # プロファイル作成
-cp ./profile/sample.yml ./profile/${STAGE_NAME}.yml
-vim ./profile/${STAGE_NAME}.yml
+cp ./sls/profile/sample.yml ./sls/profile/${STAGE_NAME}.yml
+vim ./sls/profile/${STAGE_NAME}.yml
 
 # デプロイ
-sls deploy --stage ${STAGE_NAME}
+./bin/sls -- deploy --stage ${STAGE_NAME}
 
 # 削除
-sls remove --stage ${STAGE_NAME}
+./bin/sls -- remove --stage ${STAGE_NAME}
 ```
 
 # 開発環境
---
+++ bin/sls.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+function usage {
+cat >&2 <<EOF
+slsコマンド
+
+[usage]
+ $0 [options] -- [SLS_ARG1] [SLS_ARG2] [SLS_ARG3] ...
+
+[options]
+ -h | --help:
+   ヘルプを表示
+ --profile <AWS_PROFILE>:
+   awsのプロファイル名を指定 (default=default)
+ --region <AWS_REGION>:
+   awsのリージョンを指定 (default=ap-northeast-1)
+ --proxy:
+   プロキシ設定を有効化
+
+[example]
+  ヘルプ
+    $0 -- help
+  デプロイ
+    $0 -- deploy --stage dev
+  デプロイステータス
+    $0 -- info --stage dev
+  削除
+    $0 -- remove --stage dev
+EOF
+exit 1
+}
+
+PROJECT_ROOT="$(cd $(dirname $0)/..; pwd)"
+source "${PROJECT_ROOT}/bin/lib/utils.sh"
+
+APP_NAME=$(get_app_name ${PROJECT_ROOT}/app_name)
+
+AWS_PROFILE="default"
+AWS_REGION="ap-northeast-1"
+BUILD_OPTIONS=
+SLS_ARGS=()
+while [ "$#" != 0 ]; do
+  case $1 in
+    -h | --help ) usage;;
+    --proxy     ) BUILD_OPTIONS="$BUILD_OPTIONS --build-arg proxy=$proxy --build-arg no_proxy=$no_proxy";;
+    --profile   ) shift;AWS_PROFILE="$1";;
+    --region    ) shift;AWS_REGION="$1";;
+    --          ) shift; SLS_ARGS+=($@); break;;
+    -* | --*    ) error "$1 : 不正なオプションです";;
+  esac
+  shift
+done
+
+BUILD_OPTIONS="$BUILD_OPTIONS --build-arg host_uid=$(id -u)"
+BUILD_OPTIONS="$BUILD_OPTIONS --build-arg host_gid=$(id -g)"
+AWS_ACCESS_KEY_ID=$(aws --profile $AWS_PROFILE --region $AWS_REGION configure get aws_access_key_id)
+AWS_SECRET_ACCESS_KEY=$(aws --profile $AWS_PROFILE --region $AWS_REGION configure get aws_secret_access_key)
+
+invoke docker build $BUILD_OPTIONS -q --rm -f docker/sls/Dockerfile -t ${APP_NAME}/sls:latest .
+
+#!/bin/bash
+PROJECT_ROOT=$(cd $(dirname $0)/..; pwd)
+#  -v ${HOME}/.aws:/root/.aws:ro \
+invoke docker run -ti --rm \
+  --user app \
+  -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
+  -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v ${HOME}/.cache:/home/app/.cache \
+  -v ${PROJECT_ROOT}/sls:/opt/sls \
+  ${APP_NAME}/sls:latest \
+  sls ${SLS_ARGS[@]}
